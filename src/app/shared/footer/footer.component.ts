0c143001064f398f49d551730114da07bf10fc59
Geoff <gm@sky.garden>
2019-06-12 16:29:16 +0300

    Update to footer date

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+  public currentYear: Date;
   constructor() { }
 
   ngOnInit() {
+    this.currentYear = new Date();
   }
 
 }
